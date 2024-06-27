d5fc97c47aa5493075e8513fc1610e75ed9c8396
roimenashe <roimenashe5@gmail.com>
2024-06-27 15:42:44 +0300

    Project initial commit

---
+++ src/test/java/MetadataTests.java
@@ -0,0 +1,16 @@
+import com.aerospike.ConditionTranslator;
+import com.aerospike.client.exp.Expression;
+import org.junit.jupiter.api.Test;
+
+public class MetadataTests {
+
+    @Test
+    void testDeviceSizeAndTtl() {
+        translateAndPrint("$.deviceSize() > 1024 and $.ttl() < 300");
+    }
+
+    private void translateAndPrint(String input) {
+        Expression expression = ConditionTranslator.translate(input);
+        System.out.println(expression);
+    }
+}
