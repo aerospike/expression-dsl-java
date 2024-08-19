96de06c82e402a043f610b81fd493c96883e7507
roimenashe <roimenashe5@gmail.com>
2024-08-20 00:14:28 +0300

    Add comment

--- src/main/java/com/aerospike/dsl/model/cdt/map/MapKeyBin.java
+++ src/main/java/com/aerospike/dsl/model/cdt/map/MapKeyBin.java
@@ -5,6 +5,10 @@ import com.aerospike.client.exp.Exp;
 import com.aerospike.dsl.model.BasePath;
 import lombok.Getter;
 
+/**
+ * Acts as a synthetic Map part, used for map.size() scenarios when we don't have any parts.
+ * This is not needed in lists because list have [] for list bins.
+ */
 @Getter
 public class MapKeyBin extends MapPart {
     private final String key;
