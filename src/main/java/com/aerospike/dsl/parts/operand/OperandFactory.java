b7510a758ff3e9e7ea1ec8bdaa3beda6a833a772
Andrey G <agrgr@users.noreply.github.com>
2025-09-26 21:41:24 +0200

    Add name field to Index object, fix javadoc (#50)

--- src/main/java/com/aerospike/dsl/parts/operand/OperandFactory.java
+++ src/main/java/com/aerospike/dsl/parts/operand/OperandFactory.java
@@ -8,7 +8,7 @@ import com.aerospike.dsl.parts.AbstractPart;
  * This factory provides a static method to dynamically create concrete operand implementations
  * such as {@link StringOperand}, {@link BooleanOperand}, {@link FloatOperand}, and {@link IntOperand}
  * from various Java primitive and wrapper types. It centralizes the logic for type-specific object creation.
- * </p>
+ *
  * @see StringOperand
  * @see BooleanOperand
  * @see FloatOperand
@@ -26,10 +26,10 @@ public interface OperandFactory {
      * <li>{@link Float} or {@link Double} to {@link FloatOperand}.</li>
      * <li>{@link Integer} or {@link Long} to {@link IntOperand}.</li>
      * </ul>
-     * </p>
+     *
      * @param value The object to be converted into an operand. This cannot be {@code null}.
      * @return A new instance of an operand that extends {@link AbstractPart}.
-     * @throws IllegalArgumentException If the value provided is {@code null}.
+     * @throws IllegalArgumentException      If the value provided is {@code null}.
      * @throws UnsupportedOperationException If the type of the value is not supported by the factory.
      */
     static AbstractPart createOperand(Object value) {
@@ -47,7 +47,7 @@ public interface OperandFactory {
             return new IntOperand(((Number) value).longValue());
         } else {
             throw new UnsupportedOperationException(String.format("Cannot create operand from value of type %s, " +
-                    "only String, boolean, float, double, long and integer values are currently supported",
+                            "only String, boolean, float, double, long and integer values are currently supported",
                     value.getClass().getSimpleName()));
         }
     }
